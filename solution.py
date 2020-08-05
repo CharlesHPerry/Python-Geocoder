2bff6ed009c4a23bb74c898321cf7347539e064d
charles <chuckhperry@gmail.com>
2020-08-05 13:06:33 -0700

    Part two should be done, took out API key

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import geocoder
+import requests
 
 
 
@@ -16,7 +17,11 @@ destinations = [
     "Banff National Park",
     "Capilano Suspension Bridge",
 ]
-
+API_BASE_URl = "redacted"
 for point in destinations:
     g = geocoder.arcgis(point)
-    print(f'{point} is located at {g.latlng}')
+    full_api_url = f'{API_BASE_URl}{g.lat},{g.lng}'
+    result = requests.request('GET', full_api_url).json()
+    currently = result['currently']['summary']
+    temperature = result['currently']['summary']
+    print(f'{point} is located at {g.latlng} \n The weather is currently {currently} with a temperature of {temperature}')
